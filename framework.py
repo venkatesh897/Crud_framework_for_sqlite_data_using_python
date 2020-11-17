dfe0a2796c47b72564cd8425e242f5747fda7c91
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-17 11:47:40 +0530

    Update framework.py

--- framework.py
+++ framework.py
@@ -1,11 +1,70 @@
 import sqlite3
-data_base = 'bank.db'
-
-table = input("Enter table: ").upper()
-
-connection = sqlite3.connect(data_base)
+database = 'big_bazar.db'
 menu_file = "Menu.cfg"
 record_not_found = 'Record not found.'
+
+connection = sqlite3.connect(database)
+
+tables = []
+
+def get_count_of_tables():
+	count_of_tables = 0
+
+	cursor = connection.cursor()
+
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+	tables = cursor.fetchall()
+
+	for table in tables:
+		count_of_tables = count_of_tables + 1
+
+	print(count_of_tables)
+
+def get_tables():
+
+
+	cursor = connection.cursor()
+
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+	tables = cursor.fetchall()
+
+	connection.commit()
+	return tables
+
+tables = get_tables()
+
+def show_tables():
+
+	counter = 1
+
+	for table in tables:
+		print(str(counter) + "." + str(tables[counter - 1][0]))
+		counter = counter + 1
+
+def select_table():
+
+	try:
+		user_option = int(input("Enter option: "))
+
+	except:
+		print("INVALID INPUT")
+
+	table = tables[user_option - 1][0]
+
+	return table
+
+def get_no_of_fields():
+	count_of_fields = 0
+	for column_name in column_names:
+		count_of_fields = count_of_fields + 1
+	return count_of_fields
+
+show_tables()
+
+table = select_table()
+print(table)
 
 try:
 	with open(menu_file) as f_menu:
@@ -19,52 +78,61 @@ try:
 except sqlite3.OperationalError:
 	print("Table not found.")
 
+column_names = [i[0] for i in get_column_names.description]
 
-column_name_names = [i[0] for i in get_column_names.description]
-
-max_length_column_name = column_name_names[0]
-for column_name in column_name_names:
+max_length_column_name = column_names[0]
+for column_name in column_names:
 	if(len(max_length_column_name) < len(column_name)):
 		max_length_column_name = column_name
 
-def get_no_of_fields():
-	count_of_fields = 0
-	for column_name in column_name_names:
-		count_of_fields = count_of_fields + 1
-	return count_of_fields
+max_length_column_name = column_names[0]
+
+for column_name in column_names:
+	if(len(max_length_column_name) < len(column_name)):
+		max_length_column_name = column_name
 
 def print_pipe():
 	count_of_fields = get_no_of_fields()
-	print("-" * (((len(max_length_column_name) + 6) * count_of_fields) + 1) )
+	print("-" * (((len(max_length_column_name) + 12) * count_of_fields) + 1) )
 
 def get_column_names():
 	print("|", end = "")
-	for column_name in column_name_names:
+	for column_name in column_names:
 		print(column_name, end ="")
-		print(" " * (len(max_length_column_name) - len(column_name) + 5), end = "")
+		print(" " * (len(max_length_column_name) - len(column_name) + 11), end = "")
 		print("|", end = "")
 	print("\t")
 
+def column_names_string():
+	columns_string = "("
+	for column_name in column_names:
+		columns_string = columns_string +  column_name + ","
+	columns_string = columns_string.rstrip(",") + ")"
+	return columns_string
+
 def insert_record():
-	connection = sqlite3.connect(data_base)
-	query = ('INSERT INTO %s (STATUS, ID, NAME, BALANCE) '
-	         'VALUES (:STATUS, :ID, :NAME, :BALANCE);' %(table))
-	params = {
-	        'STATUS': 'ACTIVE',
-	        'ID': input("Enter ID:"),
-	        'NAME': input("Enter name: "),
-	        'BALANCE': input("Enter balance: "),
-	    	}
+
+	field_values = []
+	for column_name in column_names:
+		if column_name == 'STATUS':
+			status = 'ACTIVE'
+			field_values.append(status)
+		else:
+			field_value = input("Enter " + column_name + ": ")
+			field_values.append(field_value)
+	record = tuple(field_values)
+	duple_of_column_names = tuple(column_names)
+	columns_string = column_names_string()
 	try:
-		connection.execute(query, params)
-		connection.commit()
-		print("Record saved successfully.")
+		is_record_saved = connection.execute("INSERT INTO " + table + " "+ columns_string + "VALUES" + str(record) ).rowcount
 	except sqlite3.IntegrityError:
-		print("ID already exist.")
-	connection.close()
+		is_record_saved = 0
+		print("Id already exist")
+	if is_record_saved > 0:
+		print("Record saved successfully.")
+	connection.commit()
 
 def show_records():
-	connection = sqlite3.connect(data_base)
 	cursor = connection.execute("SELECT * from %s WHERE STATUS = 'ACTIVE'" %(table))
 	data = cursor.fetchall()
 	print_pipe()
@@ -74,34 +142,12 @@ def show_records():
 		print("|", end ="")
 		for counter in range(0,len(record)):
 			print(record[counter], end= "")
-			print(" " * (len(max_length_column_name) - int(len(str(record[counter]))) + 5), end = "")
+			print(" " * (len(max_length_column_name) - int(len(str(record[counter]))) + 11), end = "")
 			print("|", end="")
 		print("\t")
 	print_pipe()
-	connection.close()
-
-def deactivate_record():
-	connection = sqlite3.connect(data_base)
-	user_input_id =int(input("Enter ID: "))
-	id = connection.execute("SELECT ID from %s WHERE STATUS = 'ACTIVE'" %(table))
-	is_record_found = False
-	ids = id.fetchall()
-	is_record_found = False
-	for id in ids:
-		if (id[0]) == user_input_id:
-			is_record_found = True
-			connection.execute("UPDATE %s set STATUS = 'INACTIVE' where ID =" %(table) + str(user_input_id))
-			connection.commit()
-			connection.close()
-			print("Record deleted successfully.")
-			break
-		else:
-			is_record_found = False
-	if is_record_found == False:
-		print(record_not_found)
 
 def show_record():
-	connection = sqlite3.connect(data_base)
 	user_input_id = int(input("Enter ID: "))
 	id = connection.execute("SELECT ID from %s WHERE STATUS = 'ACTIVE'" %(table))
 	ids = id.fetchall()
@@ -118,22 +164,25 @@ def show_record():
 				print("|", end ="")
 				for counter in range(0,len(record)):
 					print(record[counter], end= "")
-					print(" " * (len(max_length_column_name) - int(len(str(record[counter]))) + 5), end = "")
-					print("|", end="")
+					print(" " * (len(max_length_column_name) - int(len(str(record[counter]))) + 11), end = "")
+					print("|", end = "")
 				print("\t")
 			print_pipe()
-			connection.close()
 			break
 		else:
 			is_record_found = False
 	if is_record_found == False:
 		print(record_not_found)
-			
+
+def delete_record():
+	user_input_id =int(input("Enter ID: "))
+	is_record_deleted = connection.execute("UPDATE %s set STATUS = 'INACTIVE' where ID =" %(table) + str(user_input_id)).rowcount
+	print(is_record_deleted)
+	connection.commit()
+
 def update_record():
-	connection = sqlite3.connect(data_base)
 	count_of_fields = get_no_of_fields()
 	user_input_id = int(input("Enter ID: "))
-	count_of_fields = get_no_of_fields()
 	id = connection.execute("SELECT ID from %s WHERE STATUS = 'ACTIVE'" %(table))
 	ids = id.fetchall()
 	is_record_found = False
@@ -141,13 +190,12 @@ def update_record():
 		if id[0] == user_input_id:
 			is_record_found = True
 			for counter in range(2, count_of_fields):
-				print(str(counter - 1) + ".Update " + column_name_names[counter])
+				print(str(counter - 1) + ".Update " + column_names[counter])
 			user_input = int(input("Enter option: "))
 			if user_input >= 1 and user_input <= count_of_fields - 2:
-				user_input_data = input("Enter "+ column_name_names[user_input + 1] + ": ")
-				connection.execute("UPDATE %s set %s = '%s' where id = %s" %(table, column_name_names[user_input + 1], user_input_data, user_input_id))
+				user_input_data = input("Enter "+ column_names[user_input + 1] + ": ")
+				is_record_updated =  connection.execute("UPDATE %s set %s = '%s' where id = %s" %(table, column_names[user_input + 1], user_input_data, user_input_id))
 				connection.commit()
-				connection.close()
 				print("Record updated successfully.")
 				break
 		else:
@@ -155,7 +203,7 @@ def update_record():
 	if is_record_found == False:
 		print(record_not_found)
 
-functions_list = [insert_record, show_records, show_record, update_record, deactivate_record, exit]
+functions_list = [insert_record, show_records, show_record, update_record, delete_record, exit]
 
 while True:
 	print(menu)
@@ -174,3 +222,4 @@ while True:
 	else:
 		print("INVALID INPUT")
 
+connection.close()
